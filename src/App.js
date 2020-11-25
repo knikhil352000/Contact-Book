4c474af00d210acabc760401cf75dd5d1c38aa68
Kumar Nikhil <knikhil352000@gmail.com>
2020-11-26 02:22:12 +0530

    Added Contact Reducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/Index.css';
+import Contact from './components/contacts/Contact';
 import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Contact />
     </div>
   );
 }
